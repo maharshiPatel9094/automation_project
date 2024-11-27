efd1efbfa28d7967080979292b1100440a3ac67e
Maharshi Patel <144848033+maharshiPatel9094@users.noreply.github.com>
2024-11-26 23:21:28 -0400

    commit
    automating the clicks and writing inputs.

--- selenium_learning/app.py
+++ selenium_learning/app.py
@@ -1,5 +1,6 @@
 from selenium import webdriver #get webdriver from selenium 
 from selenium.webdriver.common.by import By #get the By 
+from selenium.webdriver.common.keys import Keys
 
 URL = "https://en.wikipedia.org/wiki/Main_Page" #wikipedia page url
 
@@ -9,6 +10,7 @@ chrome_options.add_experimental_option("detach",True)
 
 
 driver = webdriver.Chrome(options= chrome_options) #drive the chrome browser & webdriver object
+driver.maximize_window() #maximise the window to prevent unnecessary pop ups hiding the page 
 driver.get(URL) #get the url data 
 
 
@@ -17,4 +19,18 @@ articlecount = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
 print(articlecount.text) #get the number of total articles from the wikipedia page
 
 
-driver.quit() #make sure we exit all the tabs
+# articlecount.click() #clicking to elemnts using selenium driver 
+
+
+# using link text to find the element 
+# just write the same name as in the browser to select the particular link element
+# link text is specifically designed for selecting the linked elements 
+all_portals = driver.find_element(By.LINK_TEXT, value= "Community portal")
+# all_portals.click()
+
+search = driver.find_element(By.NAME,value= "search")
+search.send_keys("Python",Keys.ENTER) #write the input
+
+
+
+# driver.quit() #make sure we exit all the tabs
